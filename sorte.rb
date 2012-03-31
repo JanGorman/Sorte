8e6acb8e807b9bdfd5cd85e71396c8e513c7cde2
Jan Gorman <gorman.jan@gmail.com>
2012-03-31 15:16:14 +0200

    meat and veg

--- sorte.rb
+++ sorte.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby-local-exec
 
 require "trollop"
+require "mp3info"
 
 opts = Trollop::options do
   banner <<-END
@@ -8,8 +9,33 @@ Sort a directory of mp3s into folders base on their tags:
 
 artist/album
 
-Usage: ./sorte [options]
+Usage: sorte.rb [options]
 where options are:
 END
   opt :dry, "Dry run", :default => false
+  opt :verbose, "Verbose", :default => false
+  opt :dir, "The directory", :type => String, :default => "."
+end
+
+def make_dir(dir)
+  FileUtils.mkdir_p dir unless File.exists? dir
+end
+
+Dir.glob("#{opts[:dir]}/*.mp3") do |f|
+  Mp3Info.open(f) do |info|
+    dir = "#{opts[:dir]}/#{info.tag.artist}/#{info.tag.album}"
+    if !opts[:dry]
+      if opts[:verbose]
+        puts "Create directory #{dir}"
+      end
+      make_dir dir
+      if opts[:verbose]
+        puts "Move file #{f} to directory #{dir}"
+      end
+      File.rename(f, "#{dir}/#{f}")
+    else
+      puts "Create directory #{dir}"
+      puts "Move file #{f} to directory #{dir}"
+    end
+  end
 end
